34aa6e55c4244a3fcbf1e0a41cfa9d3a6fe84183
Evan OShea <evanoshea21@gmail.com>
2023-04-24 19:30:18 -0700

    animation

--- comps/TechScroll.js
+++ comps/TechScroll.js
@@ -3,8 +3,28 @@ import classes from '../styles/TechScroll.module.css';
 
 const TechScroll = () => {
   return (
-    <div className={classes.main}>
-      This is tech scroll.......
+    <div className={classes.logos}>
+      <div className={classes.slide}>
+        <img src='/reactLogo.png'></img>
+        <img src='/nextLogo.png'></img>
+        <img src='/awsLogo.png'></img>
+        <img src='/githubLogo.png'></img>
+        <img src='/gitLogo.png'></img>
+        <img src='/nodeLogo.png'></img>
+        <img src='/reactNativeLogo.png'></img>
+        <img src='/sqlLogo.png'></img>
+      </div>
+
+      <div className={classes.slide}>
+        <img src='/reactLogo.png'></img>
+        <img src='/nextLogo.png'></img>
+        <img src='/awsLogo.png'></img>
+        <img src='/githubLogo.png'></img>
+        <img src='/gitLogo.png'></img>
+        <img src='/nodeLogo.png'></img>
+        <img src='/reactNativeLogo.png'></img>
+        <img src='/sqlLogo.png'></img>
+      </div>
     </div>
   )
 }
