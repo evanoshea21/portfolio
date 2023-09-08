05181652c308b209bf5f4542a57b3945b6ee22f4
Evan OShea <evanoshea21@gmail.com>
2023-09-07 18:48:24 -0700

    updated calendly link

--- comps/Hero.js
+++ comps/Hero.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import classes from '../styles/Hero.module.css';
-import $ from 'jquery';
-import { useRouter } from 'next/router';
-import { Typewriter } from 'react-simple-typewriter'
-import { TypeAnimation } from 'react-type-animation';
-
+import React from "react";
+import classes from "../styles/Hero.module.css";
+import $ from "jquery";
+import { useRouter } from "next/router";
+import { Typewriter } from "react-simple-typewriter";
+import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
-
   const router = useRouter();
 
   const [inView, setInView] = React.useState(false);
 
   React.useEffect(() => {
-    if(!window) return;
+    if (!window) return;
 
     let winHeight = $(window).height();
-    let navHeight = $('.navbar').height();
+    let navHeight = $(".navbar").height();
 
-    $('#heroDiv').height(winHeight - navHeight - 42);
+    $("#heroDiv").height(winHeight - navHeight - 42);
 
     // return () => {
 
@@ -26,76 +24,77 @@ const Hero = () => {
   });
 
   const hoverOn = () => {
-    $('#scheduleBtn').css('box-shadow', '0 0 20px purple')
-    .css('text-shadow', '0 0 20px white');
+    $("#scheduleBtn")
+      .css("box-shadow", "0 0 20px purple")
+      .css("text-shadow", "0 0 20px white");
   };
   const hoverOff = () => {
-    $('#scheduleBtn').css('box-shadow', 'none')
-    .css('text-shadow', 'none');
+    $("#scheduleBtn").css("box-shadow", "none").css("text-shadow", "none");
   };
   const openCalendly = () => {
-    router.push('https://calendly.com/your360guy/talk-with-evan-software');
+    router.push("https://calendly.com/evanoshea/talk-with-evan-software");
   };
 
   const scrollTest = () => {
-    if(!inView) {
-      $('.testDiv').css('transform', 'translateY(0%)');
-      setInView(prev => !prev);
+    if (!inView) {
+      $(".testDiv").css("transform", "translateY(0%)");
+      setInView((prev) => !prev);
     } else {
-      $('.testDiv').css('transform', 'translateY(-100%)')
-      setInView(prev => !prev);
+      $(".testDiv").css("transform", "translateY(-100%)");
+      setInView((prev) => !prev);
     }
   };
 
   return (
     <div className={classes.wrap}>
+      <div id="heroDiv" className={classes.main}>
+        <div className={classes.left}>
+          <div className={classes.title}>
+            <h1>Hi, I'm Evan</h1>
 
-    <div id='heroDiv' className={classes.main}>
-      <div className={classes.left}>
-        <div className={classes.title}>
-          <h1>Hi, I'm Evan</h1>
+            <p className={classes.typeBox}>
+              I am a
+              <span className={classes.typing}>
+                {/* <Typewriter words={['Fullstack', 'Backend', 'Creative']} loop={0} typeSpeed={80} deleteSpeed={50} /> */}
+                <TypeAnimation
+                  sequence={[
+                    "Fullstack", // Types 'One'
+                    1700, // Waits 1s
+                    "Backend", // Deletes 'One' and types 'Two'
+                    1800, // Waits 2s
+                    "Creative",
+                    1800,
+                  ]}
+                  // wrapper="span"
+                  cursor={true}
+                  repeat={Infinity}
+                  // style={{ fontSize: '2em', display: 'inline-block' }}
+                />
+              </span>
+              <br />
+              Developer
+            </p>
 
-          <p className={classes.typeBox} >I am a
-          <span className={classes.typing}>
-
-            {/* <Typewriter words={['Fullstack', 'Backend', 'Creative']} loop={0} typeSpeed={80} deleteSpeed={50} /> */}
-            <TypeAnimation
-            sequence={[
-              'Fullstack', // Types 'One'
-              1700, // Waits 1s
-              'Backend', // Deletes 'One' and types 'Two'
-              1800, // Waits 2s
-              'Creative',
-              1800
-            ]}
-            // wrapper="span"
-            cursor={true}
-            repeat={Infinity}
-            // style={{ fontSize: '2em', display: 'inline-block' }}
-          />
-
-          </span>
-          <br/>
-          Developer
-          </p>
-
-
-          <div className={classes.btnAlign}>
-            <a target="_blank" href='https://calendly.com/your360guy/talk-with-evan-software' onMouseEnter={hoverOn} onMouseLeave={hoverOff} id='scheduleBtn' className={classes.btn} >
-              <i className="fa-regular fa-calendar-days"></i>Schedule Meeting
+            <div className={classes.btnAlign}>
+              <a
+                target="_blank"
+                href="https://calendly.com/your360guy/talk-with-evan-software"
+                onMouseEnter={hoverOn}
+                onMouseLeave={hoverOff}
+                id="scheduleBtn"
+                className={classes.btn}
+              >
+                <i className="fa-regular fa-calendar-days"></i>Schedule Meeting
               </a>
+            </div>
           </div>
-
-          </div>
+        </div>
+        <div className={classes.right}>
+          <img className={classes.svg} src="dev.svg"></img>
+        </div>
       </div>
-      <div className={classes.right}>
-        <img className={classes.svg}  src='dev.svg'></img>
-      </div>
-
     </div>
-    </div>
+  );
+};
 
-  )
-}
-
-export default Hero
+export default Hero;
